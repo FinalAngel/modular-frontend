0d0aecb8bee0ec1f18055a0a80d10ccf2443d291
Angelo Dini <angelo.dini@divio.ch>
2015-09-07 18:46:52 +0200

    added excercise 3

---
+++ _master/3_js__modular.js
@@ -0,0 +1,29 @@
+/*  ############################################################################
+    Excercise 3
+    ###########
+    - Create one module which sets the "active" class to the current selected
+      "gallery" item "li".
+    - Create another module which does the same for the "pagination"
+    - Pagination will trigger a gallery change, try to communicate with
+      "gallery" without altering each others code (hint event dispatching)
+
+    <ul class="gallery">
+        <li><img src="..." /></li>
+        <li><img src="..." /></li>
+        <li><img src="..." /></li>
+    </ul>
+
+    <ol class="pagination">
+        <li><a href="#">1</a></li>
+        <li><a href="#">1</a></li>
+        <li><a href="#">1</a></li>
+    </ol>
+*/
+
+function gallery() {
+
+}
+
+function pagination() {
+
+}
