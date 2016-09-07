f4acf7c7270084f94665ca8c92831ee31f08de22
Angelo Dini <angelo.dini@divio.ch>
2016-09-07 11:09:51 +0200

    added some more clarification from feedback

--- exercises/4_js__modular.js
+++ exercises/4_js__modular.js
@@ -6,17 +6,24 @@
     - Create another module which does the same for the "pagination"
     - Pagination will trigger a gallery change, try to communicate with
       "gallery" without altering each others code (hint event dispatching)
+    - jQuery is allowed
 
     <ul class="gallery">
-        <li><img src="..."></li>
-        <li><img src="..."></li>
-        <li><img src="..."></li>
+        <li class="gallery-item" hidden>
+            <img src="http://dummyimage.com/100x100/000/fff" alt="">
+        </li>
+        <li class="gallery-item" hidden>
+            <img src="http://dummyimage.com/110x100/000/fff" alt="">
+        </li>
+        <li class="gallery-item" hidden>
+            <img src="http://dummyimage.com/120x100/000/fff" alt="">
+        </li>
     </ul>
 
     <ol class="pagination">
-        <li><a href="#">1</a></li>
-        <li><a href="#">1</a></li>
-        <li><a href="#">1</a></li>
+        <li class="pagination-item"><a href="#">1</a></li>
+        <li class="pagination-item"><a href="#">1</a></li>
+        <li class="pagination-item"><a href="#">1</a></li>
     </ol>
 */
 
