71a726111dfacad6eb4c9e3fc2e25248e1f8dca4
FinalAngel <angelo.dini@divio.ch>
2020-09-22 10:38:46 +0200

    baseline

--- ex2/src/components/logo/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-// example using css-modules
-import styles from "./logo.module.css";
-
-const Logo = () => <h1 className={styles.logo}>A Big Title</h1>;
-
-export default Logo;
--- ex3-solution/.storybook/preview.js
+++
@@ -1,8 +0,0 @@
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  options: {
-    storySort: {
-      order: ["Atoms", "Molecules", "Organisms", "Templates", "Pages"],
-    },
-  },
-};
