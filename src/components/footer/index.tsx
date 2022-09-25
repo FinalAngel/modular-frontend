0e87ffdffae5ed919fb5770004f4dfb41909a70a
Angelo Dini <angelo.dini@divio.ch>
2022-09-25 23:40:26 +0200

    and separate footer properly

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,44 +1,13 @@
 import React from "react";
 
-import QuickLinks from "components/quick-links";
-import Teaser from "components/teaser";
-
-import { Text } from "components/typography";
-import { Grid, GridItem } from "components/grid";
-
 import "./footer.scss";
 
-const Footer: React.FC = () => {
-  return (
-    <footer className="footer">
-      <Grid>
-        <GridItem alpha>
-          <QuickLinks items={["Home", "Events", "Support", "Contact"]} />
-        </GridItem>
+interface Props {
+  children?: React.ReactNode;
+}
 
-        <GridItem>
-          <Teaser title="Announcements / Promoted Content">
-            <Text>
-              Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,
-              quis gravida magna mi a libero. Suspendisse enim turpis, dictum
-              sed, iaculis a, condimentum nec, nisi. Praesent egestas tristique
-              nibh. Vestibulum suscipit nulla quis orci. Nunc nonummy metus.
-            </Text>
-          </Teaser>
-        </GridItem>
-
-        <GridItem omega>
-          <Teaser title="Coming Soon">
-            <img
-              src="https://dummyimage.com/150x60/ffffff/999999.jpg"
-              alt=""
-              className="footer__logo"
-            />
-          </Teaser>
-        </GridItem>
-      </Grid>
-    </footer>
-  );
+const Footer: React.FC<Props> = ({ children }) => {
+  return <footer className="footer">{children}</footer>;
 };
 
 export default Footer;
