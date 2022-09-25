1dfab811c77f1bab5055eb31601e612726fbe852
Angelo Dini <angelo.dini@divio.ch>
2022-09-25 22:25:22 +0200

    migrate products to be BEM friendly

--- src/components/products/index.tsx
+++ src/components/products/index.tsx
@@ -4,40 +4,64 @@ import "./products.scss";
 
 const Products: React.FC = () => {
   return (
-    <section>
-      <h2>Products</h2>
+    <section className="section">
+      <h2 className="title title--h2">Products</h2>
       <ul className="products">
-        <li className="product">
-          <img src="https://dummyimage.com/300x300/cccccc/999999.jpg" alt="" />
-          <h3 className="product-title">
-            <a href="./#">Item Title</a>
+        <li className="products__item">
+          <img
+            src="https://dummyimage.com/300x300/cccccc/999999.jpg"
+            alt=""
+            className="products__image"
+          />
+          <h3 className="products__title">
+            <a href="./#" className="products__anchor">
+              Item Title
+            </a>
           </h3>
-          <p>Item Category</p>
-          <p>$88.00</p>
+          <p className="products__category">Item Category</p>
+          <p className="products__price">$88.00</p>
         </li>
-        <li className="product">
-          <img src="https://dummyimage.com/300x300/cccccc/999999.jpg" alt="" />
-          <h3 className="product-title">
-            <a href="./#">Item Title</a>
+        <li className="products__item">
+          <img
+            src="https://dummyimage.com/300x300/cccccc/999999.jpg"
+            alt=""
+            className="products__image"
+          />
+          <h3 className="products__title">
+            <a href="./#" className="products__anchor">
+              Item Title
+            </a>
           </h3>
-          <p>Item Category</p>
-          <p>$88.00</p>
+          <p className="products__category">Item Category</p>
+          <p className="products__price">$88.00</p>
         </li>
-        <li className="product">
-          <img src="https://dummyimage.com/300x300/cccccc/999999.jpg" alt="" />
-          <h3 className="product-title">
-            <a href="./#">Item Title</a>
+        <li className="products__item">
+          <img
+            src="https://dummyimage.com/300x300/cccccc/999999.jpg"
+            alt=""
+            className="products__image"
+          />
+          <h3 className="products__title">
+            <a href="./#" className="products__anchor">
+              Item Title
+            </a>
           </h3>
-          <p>Item Category</p>
-          <p>$88.00</p>
+          <p className="products__category">Item Category</p>
+          <p className="products__price">$88.00</p>
         </li>
-        <li className="product">
-          <img src="https://dummyimage.com/300x300/cccccc/999999.jpg" alt="" />
-          <h3 className="product-title">
-            <a href="./#">Item Title</a>
+        <li className="products__item">
+          <img
+            src="https://dummyimage.com/300x300/cccccc/999999.jpg"
+            alt=""
+            className="products__image"
+          />
+          <h3 className="products__title">
+            <a href="./#" className="products__anchor">
+              Item Title
+            </a>
           </h3>
-          <p>Item Category</p>
-          <p>$88.00</p>
+          <p className="products__category">Item Category</p>
+          <p className="products__price">$88.00</p>
         </li>
       </ul>
     </section>
