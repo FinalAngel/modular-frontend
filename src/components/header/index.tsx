d9aaf364ca1a96dad480eee6439d08c0cdcada82
Angelo Dini <angelo.dini@divio.ch>
2022-09-25 23:20:11 +0200

    our header should be its own component actually!

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,49 +1,13 @@
 import React from "react";
 
-import { Form, Input } from "components/form";
-import { Nav, NavItem, NavAnchor } from "components/nav";
-
 import "./header.scss";
 
-const menuEntries = [
-  "Home",
-  "Event",
-  "Accessoires",
-  "Apparel",
-  "Featured Items",
-];
+interface Props {
+  children?: React.ReactNode;
+}
 
-const Header: React.FC = () => {
-  return (
-    <header className="header">
-      <Nav flex className="header__nav">
-        <NavItem>
-          12 items in <NavAnchor to={"./#"}>Cart</NavAnchor>
-        </NavItem>
-        <NavItem>
-          <NavAnchor to={"./#"}>My Account</NavAnchor>
-        </NavItem>
-        <NavItem>
-          <Form inline>
-            <Input type="text" /> <Input type="submit" value="Search" />
-          </Form>
-        </NavItem>
-      </Nav>
-
-      <h1 className="logo">A Big Title</h1>
-
-      <nav className="navigation">
-        <h2 hidden>Navigation</h2>
-        <Nav>
-          {menuEntries.map((entry) => (
-            <NavItem key={entry} active>
-              <NavAnchor to={"./#"}>{entry}</NavAnchor>
-            </NavItem>
-          ))}
-        </Nav>
-      </nav>
-    </header>
-  );
+const Header: React.FC<Props> = ({ children }) => {
+  return <header className="header">{children}</header>;
 };
 
 export default Header;
--- src/index.tsx
+++ src/index.tsx
@@ -8,14 +8,52 @@ import Header from "components/header";
 import Headline from "components/headline";
 import Products from "components/products";
 
+import { Form, Input } from "components/form";
+import { Nav, NavItem, NavAnchor } from "components/nav";
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
+const menuEntries = [
+  "Home",
+  "Event",
+  "Accessoires",
+  "Apparel",
+  "Featured Items",
+];
+
 root.render(
   <React.StrictMode>
     <Container>
-      <Header />
+      <Header>
+        <Nav flex className="header__nav">
+          <NavItem>
+            12 items in <NavAnchor to={"./#"}>Cart</NavAnchor>
+          </NavItem>
+          <NavItem>
+            <NavAnchor to={"./#"}>My Account</NavAnchor>
+          </NavItem>
+          <NavItem>
+            <Form inline>
+              <Input type="text" /> <Input type="submit" value="Search" />
+            </Form>
+          </NavItem>
+        </Nav>
+
+        <h1 className="logo">A Big Title</h1>
+
+        <nav className="navigation">
+          <h2 hidden>Navigation</h2>
+          <Nav>
+            {menuEntries.map((entry) => (
+              <NavItem key={entry} active>
+                <NavAnchor to={"./#"}>{entry}</NavAnchor>
+              </NavItem>
+            ))}
+          </Nav>
+        </nav>
+      </Header>
       <Headline />
       <Products />
       <Footer />
