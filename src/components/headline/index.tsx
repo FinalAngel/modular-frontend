003c5b86a41aa48f58a2b40c27c25eeaa3e8c4a7
Angelo Dini <angelo.dini@divio.ch>
2022-09-25 22:17:43 +0200

    migrate headline to be BEM friendly

--- src/components/headline/index.tsx
+++ src/components/headline/index.tsx
@@ -4,10 +4,14 @@ import "./headline.scss";
 
 const Headline: React.FC = () => {
   return (
-    <section>
-      <img src="https://dummyimage.com/1024x300/cccccc/999999.jpg" alt="" />
-      <h2>Intro Copy Headline</h2>
-      <p>
+    <section className="headline">
+      <img
+        src="https://dummyimage.com/1024x300/cccccc/999999.jpg"
+        className="headline__image"
+        alt=""
+      />
+      <h2 className="title title--h2">Intro Copy Headline</h2>
+      <p className="text">
         Fusce vel dui. In enim justo, rhoncus ut, imperdiet a, venenatis vitae,
         justo. Pellentesque commodo eros a enim. Fusce a quam. Etiam vitae
         tortor.
