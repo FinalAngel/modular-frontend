1bd4ca76bedc121656f98c127d79e7f9980f1fc6
Angelo Dini <angelo.dini@divio.ch>
2022-09-25 22:04:41 +0200

    Migrating the remaining components to react
    Next, I migrated the PostHTML components to React to get the page rendered correctly. Now I can use npm run test again :)

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import ReactDOM from "react-dom/client";
 import Container from "components/container";
 import Copyright from "components/copyright";
 import Footer from "components/footer";
+import Header from "components/header";
+import Headline from "components/headline";
+import Products from "components/products";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -12,6 +15,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Container>
+      <Header />
+      <Headline />
+      <Products />
       <Footer />
       <Copyright />
     </Container>
