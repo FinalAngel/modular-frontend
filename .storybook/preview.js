c0abda512153034f064f4702e118a96ad7ea1b04
Angelo Dini <angelo.dini@divio.ch>
2022-09-26 16:24:09 +0200

    add decorators example and better import 🙇

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,8 @@
+import React from "react";
+import "../src/index.scss";
+
+export const decorators = [(Story) => <Story />];
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
